03e783d1bfb34c775fdfcca1976f90850f88b931
winniethepoOhXD <hannalouis2004@gmail.com>
2026-05-18 01:51:00 +0800

    Enhanced indentation and spacing

--- App/controllers/ErrorController.php
+++ App/controllers/ErrorController.php
@@ -1,34 +1,34 @@
 <?php
 
 namespace App\Controllers;
-
-class ErrorController{
+class ErrorController
+{
 
     /**
      * Error 404 not found
      * @return void
      */
-
-    public static function notFound($message = 'Page not found'){
+    public static function notFound($message = 'Page not found')
+    {
         http_response_code(404);
 
             loadView('error', [
                 'status' => '404',
                 'message' => $message
             ]);
-                }
+    }
 
      /**
      * Error 403 not unauthorized error
      * @return void
      */
-
-    public static function unauthorized($message = 'You are not authorized to view this page'){
+    public static function unauthorized($message = 'You are not authorized to view this page')
+    {
         http_response_code(403);
 
             loadView('error', [
                 'status' => '403',
                 'message' => $message
             ]);
-                }
+    }
 }
